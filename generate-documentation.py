0b0005c92c9a84dbadb2eba3fd48bc286425bf02
norsse <raphael.pastre@gmail.com>
2025-06-28 17:56:11 +0200

    Check that target changed

--- generate-documentation.py
+++ generate-documentation.py
@@ -66,7 +66,53 @@ class DocGenerator:
         else:
             print(f"Please check this sequence, it might be wrong: {sequence}")
 
+    def _get_current_target_hash(self, path: str) -> hashlib.sha256:
+        try:
+            os.path.exists(path)
+        except Exception as e:
+            raise e
+
+        with open(path, "rb") as f:
+            return hashlib.sha256(f.read()).hexdigest()
+
+    def _get_previous_target_hash(self, path: str):
+        previous_hash = ""
+        if not os.path.exists(path):
+            return previous_hash
+
+        with open(path, "rb") as file:
+            lines = file.readlines()
+            for line in lines:
+                previous_hash += line.decode("utf-8")
+
+            return previous_hash
+
+    def _write_new_target_hash(self) -> None:
+        try:
+            os.remove("previous_hash.txt")
+        except Exception as e:
+            print(e)
+            pass
+
+        with open("previous_hash.txt", "w") as file:
+            file.write(self._get_current_target_hash(SCRIPT_NAME))
+
+    def _target_hash_changed(self) -> bool:
+        current_hash = self._get_current_target_hash(SCRIPT_NAME)
+        previous_hash = self._get_previous_target_hash("previous_hash.txt")
+
+        target_has_changed = current_hash != previous_hash
+        print(target_has_changed)
+
+        if target_has_changed:
+            self._write_new_target_hash()
+
+        return target_has_changed
+
     def generate(self) -> None:
+
+        if not self._target_hash_changed():
+            return
 
         output = self._template.render(
             script_name=SCRIPT_NAME,
@@ -80,20 +126,3 @@ class DocGenerator:
 
 generator = DocGenerator()
 generator.generate()
-
-"""
-    def _get_hash(self) -> hashlib.sha256:
-
-        try:
-            os.path.exists(path)
-        except Exception as e:
-            print(e)
-
-        with open(path, "rb") as f:
-            return hashlib.sha256(f.read()).hexdigest()
-
-
-        print(os.path.exists("test.sh"))
-        print(hash_file_content("test.sh"))
-"""
-
