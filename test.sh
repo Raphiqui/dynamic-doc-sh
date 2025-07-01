78031f622244f5bb3db0489f22938a33c4c2d8fd
norsse <raphael.pastre@gmail.com>
2025-07-01 23:22:31 +0200

    Doc generated plus its hash

--- test.sh
+++ test.sh
@@ -2,21 +2,29 @@
 
 set -e
 
-sc3ui_pod_name="cmcc-sc3ui-0"
-simulator_pod_name="cmcc-rsu-simulator-0"
+# @doc: Developer settings are used in development mode, don't use it if you deploy in production
+# @default: YES
+read -e -i yes -p "Use Developer Settings [YES/no]" sc3ui_pod_name_input
+echo "Developer Settings: $sc3ui_pod_name_input"
 
-# @doc: This is an example
-# @default: cmcc-sc3ui-0
-echo "The default value for sc3ui pod name is $sc3ui_pod_name. Would you like to change it? (Press Enter to keep the default or type a new value)"
-read sc3ui_pod_name_input
+# @doc: Pick the environment which meets your needs
+# @default: dev
+read -e -i staging -p "Select environment [staging/prod/dev]" environment
+echo "Environment selected: $environment"
 
-read -e -i yes -p "Use Developer Settings [YES/no]}" sc3ui_pod_name_input
+# @doc: k8s namespace must be specified otherwise none of the kubectl command will work
+# @default: hello-world
+read -e -i default -p "Enter Kubernetes namespace" namespace
+echo "Namespace: $namespace"
 
-echo "The default value for sc3ui pod name is $simulator_pod_name. Would you like to change it? (Press Enter to keep the default or type a new value)"
-read simulator_pod_name_input
+# @doc: Pod name gives you access to a docker image
+# @default: hello
+read -e -i my-pod -p "Enter pod name" pod_name
+echo "Pod Name: $pod_name"
 
-if [ -n "$simulator_pod_name_input" ]; then
-  simulator_pod_name="$simulator_pod_name_input"
-fi
+# @doc: Container name
+# @default: my-container
+read -e -i app-container -p "Enter container name" container_name
+echo "Container Name: $container_name"
 
-echo "Now the values are $sc3ui_pod_name and $simulator_pod_name"
+echo "Running command: kubectl exec -n $namespace $pod_name -c $container_name -- echo Hello World"
---
+++ test.sh-doc.md
@@ -0,0 +1,17 @@
+# Documentation for test.sh
+
+ Developer settings are used in development mode, don't use it if you deploy in production
+- Use Developer Settings [YES/no]: YES
+
+ Pick the environment which meets your needs
+- Select environment [staging/prod/dev]: dev
+
+ k8s namespace must be specified otherwise none of the kubectl command will work
+- Enter Kubernetes namespace: hello-world
+
+ Pod name gives you access to a docker image
+- Enter pod name: hello
+
+ Container name
+- Enter container name: my-container
+
