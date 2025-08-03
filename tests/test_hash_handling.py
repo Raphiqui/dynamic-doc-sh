5a24f9b535b568454f8885fe6e24ae604311a624
norsse <raphael.pastre@gmail.com>
2025-08-03 18:34:39 +0200

    Testing generation in another file

--- tests/test_hash_handling.py
+++ tests/test_hash_handling.py
@@ -1,5 +1,4 @@
 from hash_handling import HashHandler
-import hashlib
 
 
 class TestHashHandler:
@@ -9,12 +8,3 @@ class TestHashHandler:
         assert (
             hash_handler.previous_hash_path is not None
         ), "previous_hash_path must not be empty"
-
-
-class TestGenerator:
-
-    def test_script(self, temp_files, test_script_hash):
-
-        with open(temp_files["script_file"], "rb") as file:
-            current_hash = str(hashlib.sha256(file.read()).hexdigest())
-            assert current_hash == test_script_hash, "Test script has changed somehow"
