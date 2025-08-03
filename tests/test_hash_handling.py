8551773a4c832b0b9d8fcd0739e6e438a8aacfa2
norsse <raphael.pastre@gmail.com>
2025-08-03 12:14:19 +0200

    Test hash handler instance

---
+++ tests/test_hash_handling.py
@@ -0,0 +1,10 @@
+from hash_handling import HashHandler
+
+
+class TestHashHandler:
+
+    def test_instance_is_valid(self, hash_handler: HashHandler):
+        assert hash_handler.script_path is not None, "script_path must not be empty"
+        assert (
+            hash_handler.previous_hash_path is not None
+        ), "previous_hash_path must not be emptprevious_hash_path must not be emptprevious_hash_path must not be empty"
